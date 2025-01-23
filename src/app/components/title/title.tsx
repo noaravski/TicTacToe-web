e1bb8aa7d2707abab90c60b10099244b48a1ef7c
noaravski <87970356+noaravski@users.noreply.github.com>
2025-01-23 18:20:59 +0200

    Adding title implementation for the page

---
+++ src/app/components/title/title.tsx
@@ -0,0 +1,7 @@
+export default function Title() {
+  return (
+    <div className="flex flex-col items-center justify-center h-20 w-full bg-[#00394C] text-[#ffffff] text-4xl font-bold">
+      <h1>Tic Tac Toe</h1>
+    </div>
+  );
+}
